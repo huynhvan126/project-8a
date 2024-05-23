b4c1d68d77332789a0f8e7dc3daf6b46ea8e4160
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-05-22 21:16:55 -0700

    Add files via upload

---
+++ count_letters.py
@@ -0,0 +1,15 @@
+# Author: Van Huynh
+# GitHub username: huynhvan126
+# Date: 05/12/2024
+# Description: counts only the letter in a string including upper-case and lower-case letters.
+def count_letters(string):
+    """Count the number of letters in a string."""
+    letter_counts = {}
+    for char in string:
+        if char.isalpha():
+            upper_char = char.upper()
+        if upper_char not in letter_counts:
+            letter_counts[upper_char] += 1
+        else:
+            letter_counts[upper_char] = 1
+    return letter_counts
